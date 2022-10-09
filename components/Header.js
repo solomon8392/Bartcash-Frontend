be820dd0f5def8f8be73721392ae4e13a2eadda7
Victor <ukokjnr@gmail.com>
2022-10-09 14:37:29 +0100

    Correct

--- components/Header.js
+++ components/Header.js
@@ -32,11 +32,11 @@ export default function Home() {
                     <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                   </svg>
         </button>
-        <div class="flex justify-center">
+        <div className="flex justify-center">
   <div>
-    <div class="dropdown relative">
+    <div className="dropdown relative">
       <button
-        class="
+        className="
           dropdown-toggle
           px-2
           py-2.5
@@ -58,7 +58,7 @@ export default function Home() {
           focusable="false"
           data-prefix="fas"
           data-icon="caret-down"
-          class="w-2 ml-2"
+          className="w-2 ml-2"
           role="img"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 320 512"
@@ -70,7 +70,7 @@ export default function Home() {
         </svg>
       </button>
       <ul
-        class={`
+        className={`
           dropdown-menu
           absolute
           min-w-max
@@ -95,7 +95,7 @@ export default function Home() {
       >
         <li>
           <a
-            class="
+            className="
               dropdown-item
               text-sm
               py-2
@@ -114,7 +114,7 @@ export default function Home() {
         </li>
         <li>
           <a
-            class="
+            className="
               dropdown-item
               text-sm
               py-2
@@ -133,7 +133,7 @@ export default function Home() {
         </li>
         <li>
           <a
-            class="
+            className="
               dropdown-item
               text-sm
               py-2
