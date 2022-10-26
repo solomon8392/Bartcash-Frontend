2b04f5937940c0116fe84a758246336611dd255a
Victor <ukokjnr@gmail.com>
2022-10-26 10:56:57 +0100

    Work

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['mdbootstrap.com', "localhost"],
+    domains: ['mdbootstrap.com', "localhost", "bartcash.onrender.com"],
   },
 }
 
